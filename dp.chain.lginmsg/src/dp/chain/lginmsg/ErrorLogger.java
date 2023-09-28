c130d977c13c1fda92d6aead628cfd249b13fea8
QAZI-SAMI <106952439+QAZI-SAMI@users.noreply.github.com>
2023-09-28 10:39:26 +0500

    Update ErrorLogger.java

--- dp.chain.lginmsg/src/dp/chain/lginmsg/ErrorLogger.java
+++ dp.chain.lginmsg/src/dp/chain/lginmsg/ErrorLogger.java
@@ -3,22 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dp.chain.lginmsg;
+package Logger;
 
-/**
- *
- * @author fa20-bse-024
- */
-public class ErrorLogger extends AbstractLogger  {
-    
-    public ErrorLogger(int level){
-        this.level = level;
-        
-    }
-    
-@Override
-protected void write(String message) {
-    System.out.println("Error Console : : Logger:  "+ message );
-    
-}
+public class ErrorLogger extends AbstractLogger {
+
+   public ErrorLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("Error Console::Logger: " + message);
+   }
 }
