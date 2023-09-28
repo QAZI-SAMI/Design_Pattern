8ea22c4bb7651921fbc0007074134c125e1d5f89
QAZI-SAMI <106952439+QAZI-SAMI@users.noreply.github.com>
2023-09-28 10:37:11 +0500

    Update AbstractLogger.java

--- dp.chain.lginmsg/src/dp/chain/lginmsg/AbstractLogger.java
+++ dp.chain.lginmsg/src/dp/chain/lginmsg/AbstractLogger.java
@@ -3,35 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dp.chain.lginmsg;
+package Logger;
 
-/**
- *
- * @author fa20-bse-024
- */
-public abstract  class AbstractLogger {
-    public static int INFO = 1;
-    public static int DEBUG = 2;
-    public static int ERROR = 3;
-    
-    protected int level;
-    
-    //  next elemnt in chain or responsibility
-    protected AbstractLogger nextLogger;
-    
-    public void setNextLogger(AbstractLogger nextLogger){
-        this.nextLogger = nextLogger;
-        
-    }
-    
-    public void logMessage(int level, String message){
-        if(this.level <= level){
-    write(message);
-        }
-     if(nextLogger != null){
+public abstract class AbstractLogger {
+   public static int INFO = 1;
+   public static int DEBUG = 2;
+   public static int ERROR = 3;
+   public static int WARNING = 4; 
+
+   protected int level;
+
+   //next element in chain or responsibility
+   protected AbstractLogger nextLogger;
+
+   public void setNextLogger(AbstractLogger nextLogger){
+      this.nextLogger = nextLogger;
+   }
+
+   public void logMessage(int level, String message){
+      if(this.level <= level){
+         write(message);
+      }
+      if(nextLogger !=null){
          nextLogger.logMessage(level, message);
-     }
-    }
+      }
+      
+   }
+   public void logOnceMessage(int level, String message){
+      if(this.level == level){
+         write(message);
+      }
+      else if(nextLogger !=null){
+         nextLogger.logMessage(level, message);
+      }
+      else 
+          System.out.println("Support not Available");
+   }
    
-abstract protected void write(String message);
+   abstract protected void write(String message);
+	
 }
